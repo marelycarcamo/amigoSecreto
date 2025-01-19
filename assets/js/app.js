534c67127892e19f483b1b786dd422f2e6f85af9
marely <marely.carcamo@gmail.com>
2025-01-19 06:48:21 -0300

    creada funcion crearAmigo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,16 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+$(document).ready(function () {
+
+	$('#agregarAmigo').click(function () {
+		const texto = $('#amigo').val().trim(); // Obtener texto del input
+		if (texto) { // Verificar si no está vacío
+			$('#listaAmigos').append(`<li>${texto}</li>`); // Agregar a la lista
+			$('#amigo').val(''); // Limpiar el cuadro de texto
+		} else {
+			alert('Por favor, escribe algo antes de agregar.');
+		}
+	});
+
+
+
+
+});
